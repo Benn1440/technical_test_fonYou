6a6d34b5ce780c63c8cdb5757f8ef566b0ce3582
BENEDICT OKAFOR <67696393+Benn1440@users.noreply.github.com>
2023-11-29 22:14:38 +0100

    Update save_log.sh

--- save_log.sh
+++ save_log.sh
@@ -11,6 +11,6 @@ LOG_FILE="${LOG_PATH}/sample.access.log-$(date +"%Y%m%d").log"
 
 # MySQL query to extract data from Users database table and write to the log file
 mysql -h "${DB_HOST}" -u "${DB_USER}" -p"${DB_PASSWORD}" -D "${DB_NAME}" -e \
-  "SELECT users.ID, users.Name, users.public_number, f.private_number
+  "SELECT users.ID, users.Name, users.public_number, fonyou_number.private_number
    FROM users 
    JOIN fonyou_number f ON users.public_number = f.main_number;" >> "${LOG_FILE}"
